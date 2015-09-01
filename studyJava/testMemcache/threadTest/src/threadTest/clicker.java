b0197bd37d621a962147e28b2afbbcd9bdb220e6
chaoqiang <chaoqiang@staff.weibo.com>
2015-09-01 18:26:46 +0800

    add some codes about thread test

---
+++ studyJava/testMemcache/threadTest/src/threadTest/clicker.java
@@ -0,0 +1,30 @@
+package threadTest;
+
+public class clicker implements Runnable {
+	int click = 0;
+	Thread t;
+	private volatile boolean running = true;
+	
+	//构造方法
+	public clicker( int p ) {
+		t = new Thread(this);
+		t.setPriority( p );
+	}
+	//线程入口方法
+	public void run() {
+		while(running) {
+			click ++;
+		}
+	}
+	
+	//停止线程的方法
+	public void stop() {
+		running = false;
+	}
+	
+	//启动线程
+	public void start() {
+		t.start();
+	}
+
+}
